f0c36f305d1b50916ce1503eb51cce669f6786aa
Sugishin073 <buckesbeats@gmail.com>
2024-11-02 21:43:37 +0900

    テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,16 +7,16 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: { minimum: 6 }
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'must include both letters and numbers' }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :password_confirmation, presence: true
 
   # お名前（全角）は名字と名前が必須で、全角（漢字・ひらがな・カタカナ）のみ許容
-  validates :first_name, presence: true, format: { with: /\A[一-龥ぁ-ゔァ-ヴー]+\z/, message: '全角漢字・ひらがな・カタカナを使用してください' }
-  validates :family_name, presence: true, format: { with: /\A[一-龥ぁ-ゔァ-ヴー]+\z/, message: '全角漢字・ひらがな・カタカナを使用してください' }
+  validates :first_name, presence: true, format: { with: /\A[一-龥ぁ-ゔァ-ヴー]+\z/ }
+  validates :family_name, presence: true, format: { with: /\A[一-龥ぁ-ゔァ-ヴー]+\z/ }
 
   # お名前カナ（全角）は名字と名前が必須で、全角カタカナのみ許容
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' }
-  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナを使用してください' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
 
   # 生年月日は必須
   validates :birth_date, presence: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.email }
-    password              { Faker::Internet.password(min_length: 6) }
-    password_confirmation { password }
+    password              { 'Password123' }
+    password_confirmation { 'Password123' }
+    family_name             { '山田' }
+    first_name            { '太郎' }
+    family_name_kana        { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
+    birth_date            { Faker::Date.birthday(min_age: 18, max_age: 65) }
   end
 end
