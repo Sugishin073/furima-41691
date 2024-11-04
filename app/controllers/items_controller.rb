587c68cdf2f92f95878c3a9026c4b0a7a7da53be
Sugishin073 <buckesbeats@gmail.com>
2024-11-04 22:00:34 +0900

    出品完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,20 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create!(item_params)
+    redirect_to '/'
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:category_id, :prefecture_id, :shipping_fee_id, :status_id, :waiting_date_id, :name,
+                                 :description, :price, :image).merge(user_id: current_user.id)
+  end
 end
