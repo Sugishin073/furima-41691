c61f39ae2da4b8566b31497a2459ac45be96245d
Sugishin073 <buckesbeats@gmail.com>
2024-11-08 15:49:51 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -42,7 +42,7 @@ class ItemsController < ApplicationController
                                  :description, :price, :image).merge(user_id: current_user.id)
   end
 
-  def set_tweet
+  def set_item
     @item = Item.find(params[:id])
   end
 end
