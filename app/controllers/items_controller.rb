8c6cb933bb9d856ae291b410a7e834f66c842e24
Sugishin073 <buckesbeats@gmail.com>
2024-11-08 16:22:16 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :edit]
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -33,6 +33,11 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
