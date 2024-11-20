46e2e66906c43579926a4539661ba1f129586c2d
Sugishin073 <buckesbeats@gmail.com>
2024-11-20 15:59:19 +0900

    gem 修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,12 +1,12 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
-lock 'capistrano 3.19.2'
+lock '3.19.2'
 
 # Capistranoのログの表示に利用する
 set :application, 'furima-41691'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:Sugishin073/furima-41691.git'
-set :branch, 'main'
+set :branch, 'master'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
